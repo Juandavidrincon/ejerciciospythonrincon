c3c9bab716a002e55143aeeba8baef96b7e25828
unknown <juandrincon2006@gmail.com>
2024-04-18 16:39:03 -0500

    mi primer commit

---
+++ ejercicio_1.py
@@ -0,0 +1,24 @@
+a=182
+b=12
+suma=a+b
+print("la suma es:",suma)
+
+a=80
+b=50
+resta=a-b
+print("la resta es: ",resta)
+
+a=50
+b=2
+multiplicacion=a*b
+print("la mutiplicacion es: ",multiplicacion)
+
+a=150
+b=5
+division=a/b
+print("la division es: ",division)
+
+a=123
+b=2
+divisionResiduo=a%b
+print("la division residuo es: ",divisionResiduo)
